5317071adbdc509ab7341257fc14644f93189bb4
valed-dm <dmvaled@gmail.com>
2023-07-23 20:47:57 +0300

    get_score.py connected with cache

--- helpers/get_score.py
+++ helpers/get_score.py
@@ -1,10 +1,29 @@
 """Scores API request"""
+import hashlib
+
+from cache.store import cache_get, cache_set
 
 
-def get_score(store, phone, email, birthday=None, gender=None, first_name=None, last_name=None):
+def get_score(
+    phone, email, birthday=None, gender=None, first_name=None, last_name=None
+):
     """Scoring calculations for non-empty request fields"""
 
-    score = 0
+    key_parts = [
+        first_name or "",
+        last_name or "",
+        str(phone) or "",
+        birthday if birthday is not None else "",
+    ]
+    key = "uid:" + hashlib.md5("".join(key_parts).encode(encoding="utf-8")).hexdigest()
+
+    # try get from cache,
+    # fallback to heavy calculation in case of cache miss
+    score = cache_get(key) or 0
+
+    if score:
+        # print("score from cache ===>", score)
+        return score
 
     if phone:
         score += 1.5
@@ -14,5 +33,8 @@ def get_score(store, phone, email, birthday=None, gender=None, first_name=None, 
         score += 1.5
     if first_name and last_name:
         score += 0.5
+
+    # cache for 60 minutes
+    cache_set(key=key, expire=60 * 60, value=score)
 
     return score
